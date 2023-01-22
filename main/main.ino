00c57c41c7dcf7810c27815fa569c7564dbfe853
dosc <gamingmaximaal@gmail.com>
2023-01-22 21:13:39 +0100

    the note can now be changed based on a message

--- main/main.ino
+++ main/main.ino
@@ -2,6 +2,7 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include "src/SoundGenerator/sound_generator.h"
+#include <Wire.h>
 
 // IOs used for I2S. Not defined in i2s.h, unfortunately.
 // Note these are internal GPIO numbers and not pins on an
@@ -24,6 +25,10 @@
 // playback vars
 const uint32_t sample_rate = 32000;
 
+#define SLAVE_ADDR 9
+const char term = '\n';
+
+
 
 void setup()
 {
@@ -36,6 +41,9 @@ void setup()
   // start serial
   Serial.begin(115200);
   while (!Serial);
+
+  // i2c
+  Wire.begin(SLAVE_ADDR);
 
   // start i2s
   i2s_begin();
@@ -58,16 +66,34 @@ float offset = 0;
 double dSample=0;
 
 int16_t buffer[512];
+int noteIndex=0;
+int state=0;
+double notemap[24] = {
+  261.626,277.183,293.665,311.127,329.628,349.228,369.994,391.995,415.305,440.00,466.164,493.883,
+  523.251,554.365,587.330,622.254,659.225,698.456,739.989,783.991,830.609,880.00,932.328,987.767};
+
 
 void loop()
 {
+  if (Wire.available()) {
+    String ms = Wire.readStringUntil(term);
+    noteIndex = ms.substring(0,2).toInt();
+    state = ms.substring(2,3).toInt();
+  }
+
+
+  // audio stuff
   for(int i=0;i<512;i++) {
-    dSample = sineWave(440.0, offset);
-    buffer[i] = double2int(dSample*32767*volume);
-    offset+=31.25*0.000001; 
-    if (offset>1) {
-      offset = offset-1;
-    }    
+    if (noteIndex>0) {
+      dSample = sineWave(notemap[noteIndex], offset);
+      buffer[i] = double2int(dSample*32767*volume);
+      offset+=31.25*0.000001; 
+      if (offset>1) {
+        offset = offset-1;
+      }    
+    } else {
+      buffer[i] = 0;
+    }
   }
   
   for (int i=0; i<512;i++) {
